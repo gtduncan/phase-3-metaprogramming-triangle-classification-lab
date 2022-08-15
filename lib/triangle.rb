18a30625602ffefa9b3c317dc78bb23229a2385d
George Duncan <gtduncan@usc.edu>
2022-08-15 11:14:38 -0400

    commit tree 4bead6fafe5146909aa2b879df4ae7eb79128941

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,43 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+  def kind
+    validate_triangle
+    if side1 == side2 && side2 == side3
+      :equilateral
+    elsif side1 == side2 || side2 == side3 || side1 == side3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+  class TriangleError < StandardError
+  end
+
+  def greater_than_zero
+    if side1 > 0 && side2 > 0 && side3 > 0
+      true
+    else
+      false
+    end
+  end
+  
+  def triange_inequality
+    if side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1
+      true
+    else
+      false
+    end
+  end
+
+  def validate_triangle
+    if(!greater_than_zero || !triange_inequality)
+      raise TriangleError
+    end
+  end
+    
 end
